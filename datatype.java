81a530a9004e78e56b5f856b0355f410dfec2c49
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-08 09:43:35 -0700

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,52 @@
+public class datatype {
+    public static void main(String[] args) {
+        int intvalue=100;
+        System.out.println("intvalue is:"+intvalue);
+
+
+
+        byte MYNUMBERS = 100;  
+        { 
+        System.out.println(MYNUMBERS);
+
+        }
+
+         byte a = 30;
+        byte b = 20;
+        // perform arithmetic operator
+        byte number = (byte) (a-b);
+        
+            System.out.println("number:  " + number);
+
+
+          float floatvalue = 23.654f;
+          System.out.println("float value is:"+floatvalue);
+
+         
+          char letter = 'a';
+       char digit = '5';
+       System.out.println(letter + digit);
+
+char myGrade = 'a';
+     System.out.println(myGrade);
+
+     
+     char f = 'f';
+     char c = 'c';
+     System.out.println(b < c);
+     System.out.println(c > b);
+
+
+     double doublevalue = 21.56;
+     System.out.println(doublevalue);
+
+
+
+     long longvalue = 79043092;
+     System.out.println(longvalue);
+            
+        }
+    }
+    
+    
+
